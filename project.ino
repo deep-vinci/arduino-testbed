7b5f036313488a2bebc8e9053e7badde44d38b53
deep-vinci <deepakworkpc@gmail.com>
2026-04-07 23:53:48 +0530

    feat: siren

--- project.ino
+++ project.ino
@@ -1,16 +1,20 @@
-void setup() {
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-}
+// // Declare variables here so both setup() and loop() can see them
+int pin1 = 11; 
+int pin2 = 49;
 
-void loop() {
-  // LED 1 on, LED 2 off
-  digitalWrite(11, HIGH);
-  digitalWrite(10, LOW);
-  delay(250);
+void setup() { 
+  pinMode(11, OUTPUT); 
+  pinMode(49, OUTPUT); 
+} 
 
-  // LED 2 on, LED 1 off
-  digitalWrite(11, LOW);
-  digitalWrite(10, HIGH);
-  delay(250);
+void loop() { 
+  // LED 1 on, LED 2 off 
+  digitalWrite(11, HIGH); 
+  digitalWrite(49, LOW); 
+  delay(400); 
+  
+  // LED 2 on, LED 1 off 
+  digitalWrite(11, LOW); 
+  digitalWrite(49, HIGH); 
+  delay(400); 
 }
